e3e96d89a7215bd769f02742be3477d02a97320d
Paul Vollmer <wrongentertainment@yahoo.de>
2013-03-01 17:24:12 +0100

    fixed test string

--- test/serious-calculations-test.js
+++ test/serious-calculations-test.js
@@ -5,22 +5,31 @@ var assert = require('assert'),
 
 
 vows.describe('serious-calculations').addBatch({
-
   'When performing serious calculations': {
     topic: seriousCalculations.performSeriousCalculations(4),
     'result should be valid': function (result) {
       assert.isNumber(result);
       assert.equal(result, 8);
     }
-  }
+  },
 
-  'When performing an other calculations': {
-    topic: seriousCalculations.wng_hello_world('foo'),
+
+  /*'Wng Hello World': {
+    topic: seriousCalculations.performWngHelloWorld(4),
+    'result should be valid': function (result) {
+      assert.isNumber(result);
+      assert.equal(result, 4);
+    }
+  },*/
+
+  'Wng Hello World': {
+    topic: seriousCalculations.performWngHelloWorld('foo'),
     'result should be valid': function (result) {
       assert.isString(result);
       assert.equal(result, 'class { foo }');
     }
   }
 
+  
 }).export(module);
 
